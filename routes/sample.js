07e0cac57f5d6e0a03eabadce673cf1871345d5b
RobsJ13 <129525698+RobsJ13@users.noreply.github.com>
2023-06-15 17:55:28 +0530

    Initial commit

---
+++ routes/sample.js
@@ -0,0 +1,53 @@
+const express= require('express');
+const router = express.Router();
+const fs=require('fs');
+
+const path = './data.json';
+
+router.use(express.json());
+router.use(express.urlencoded({extended:true}));
+
+router.get('/',(req,res)=>{
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+          console.error("Error" ,err);
+          return res.status(500).send('Error reading hospitals data');
+        }
+        const hospitals = JSON.parse(data);
+        res.json(hospitals);  
+      });
+})
+
+router.post('/add',(req,res)=>{
+    //res.send(`hi this is post request`);
+    //console.log(req.body);
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+          console.error("Error" ,err);
+          return res.status(500).send('Error reading hospitals data');
+        }
+        const hospitals = JSON.parse(data);
+        hospitals.push(req.body) 
+        var x=JSON.stringify(hospitals)
+        fs.writeFile(path, x, (err) => {
+        if (err)
+          console.log(err);
+        else {
+        res.send(x)
+        }
+
+})
+})
+})
+
+router.put('/edit',(req,res)=>{
+    res.send(`hi this is put request`);
+})
+
+router.delete('/del',(req,res)=>{
+    res.send(`hi this is del request`);
+})
+
+
+
+module.exports=router; 
